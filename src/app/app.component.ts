8816a2607fbcef997d52b938f94836378e559c8c
Guilherme-mendesc <guimendes1337@gmail.com>
2021-06-24 12:49:19 -0300

    first project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blogGuilhermeMCDev';
+  title = 'Blog pessoal do Dev. Guilherme Mendes Carvalho';
 }
